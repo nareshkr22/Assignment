1bdd8bc3c88a4203b8da108c5a65401c1eacc63c
Naresh <nareshkr.22@gmail.com>
2018-10-29 00:19:41 +0530

    Update

--- Javascript/assign 3/change_maker/make_change.js
+++ Javascript/assign 3/change_maker/make_change.js
@@ -3,9 +3,11 @@ var $ = function(id) {
 };
 
 var processEntry = function(){
-var cents = parseFloat($("cents").value);
-if(isNaN(cents) || cents <=0 && cents >=100)
+var cents = parseInt($("cents").value);
+if(isNaN(cents) || cents <=0 || cents >=100)
 {
+
+	console.log('e');
 	alert("Sorry");
 }
 else
@@ -16,15 +18,26 @@ else
 
 function makeChange(cents)
 {
-var quarter = parseInt(cents/25); 
+var quarter = parseInt(cents/25);
+console.log("quater" + quarter) ;
 cents = cents - 25*quarter;
  
 
+
+
 var dimes = parseInt(cents/10);
+
+console.log("dimes" + dimes) ;
 cents = cents - 10*dimes;
 
 var nickel = parseInt(cents/5);
+console.log("nickel" + nickel);
+
 cents = cents - 5*nickel;
+console.log("cents" + cents);
+
+
+
 
 }
 
--- Javascript/assign 3/income_tax/calculate_tax.js
+++ Javascript/assign 3/income_tax/calculate_tax.js
@@ -5,6 +5,60 @@ var $ = function (id) {
 
 
 
+var processEntry = function(){
+var income = parseInt($("income").value);
+if(isNaN(income))
+{
+ 
+	alert("Sorry");
+}
+else
+{
+	get_tax(income);
+}
+
+}
+
+function get_tax(income)
+{
+	var tax = 0;
+	if(income >=0 && income < 9275)
+	{
+		tax = 0 + (0.10 * income)
+	}
+	else if(income >=9275 && income < 37650)
+	{
+		tax = 927.5 + (0.15 * (income - 9275));
+	}
+	else if(income >=37650 && income < 91150)
+	{
+		tax = 5183.5 + (0.25 * (income - 37650));
+	}
+	else if(income >=91150 && income < 190150)
+	{
+		tax = 18558.75 + (0.28 * (income - 91150));
+	}
+	else if(income >=190150 && income < 413350)
+	{
+		tax = 46278.75 + (0.33 * (income - 190150));
+	}
+	else if(income >=413350 && income < 415050)
+	{
+		tax = 119934.75 + (0.35 * (income - 413350));
+	}
+	else if(income >=415050)
+	{
+		tax = 120529.75 + (0.396 * (income - 415050));
+	}
+	else
+	{
+
+
+	}
+
+
+console.log(tax);
+}
 window.onload = function () {
     $("calculate").onclick = processEntry;
 };
